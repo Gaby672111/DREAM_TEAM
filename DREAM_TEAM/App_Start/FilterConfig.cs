5dad2f5d77ea3bac7b5904494ecb534bb74b1e8d
Gabri <Gabri@DESKTOP-QOTC155>
2024-03-21 18:20:43 -0700

    Add project files.

---
+++ DREAM_TEAM/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace DREAM_TEAM
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
